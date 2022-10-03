e0b7c43d5def22bc49500f4352859dfd52fc6cfa
kallagoutham <kallagoutham33@gmail.com>
2022-10-03 10:27:00 +0530

    commit

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -14,9 +14,20 @@ import java.util.Optional;
  */
 @Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private  final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> optionalCategory= categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional=unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("Cat Id is :"+ optionalCategory.get().getId());
+        System.out.println("UID is :" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
